c474ddb140bd8aeca248985e9c16b4535784a8ff
saragiselle <sarigise.miranda@gmail.com>
2024-02-15 21:37:13 -0300

    Creamos ciclos for en varios lenguajes

---
+++ 03_ITERADORES_CICLOS/ciclo_for.py
@@ -0,0 +1,21 @@
+"""
+CIclo for en otros lenguajes
+for (inicializador; condicion; iterador){
+    instruccion
+}
+
+Ciclo for en JS
+let nombres  = ["Paco", "Emilio", "Javier"]
+for (let i = 0; i < 0 nombres.length; i +=1) {
+    console.log(nombres[i])
+}
+
+Ciclo for en Python
+for elemento in list:
+    instruccion
+
+"""
+
+nombres  = ["Paco", "Emilio", "Javier"]
+for elemento in nombres:
+    print(elemento)
