ee3d23f6df40f995bcc9188b5927fae16e9580f2
saragiselle <sarigise.miranda@gmail.com>
2024-02-20 21:41:27 -0300

    Aplicar funciones a una lista por elos metodos pythonicos

---
+++ 04_LIST_COMPREHENSION/list_comprehension.py
@@ -0,0 +1,15 @@
+"""lista = [expresion (elemento) for elemento in objeto_iterable]"""
+
+def calcular_cuadrado(numero):
+    return numero**2
+
+lista_num = [1,2,3,4,5,6,7,8,9,10]
+lista_cuadrados=[]
+for num in lista_num:
+    cuadrado = num ** 2
+    lista_cuadrados.append(cuadrado)
+
+print("Ciclo for", lista_cuadrados)
+
+list_comprehension = [calcular_cuadrado(num) for num in lista_num]
+print("List comprehension", list_comprehension)
