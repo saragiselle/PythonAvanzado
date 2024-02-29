a53b7bf5912f44338150a90eae403b74c2978b6e
saragiselle <sarigise.miranda@gmail.com>
2024-02-28 21:28:10 -0300

    utilizacion de operador walrus en if

---
+++ 04_LIST_COMPREHENSION/walrus.py
@@ -0,0 +1,15 @@
+def calcular_cuadrado(numero):
+    return numero**2
+
+lista_num = [1,2,3,4,5,6,7,8,9,10]
+
+lista_pares = []
+for num in lista_num:
+    """cuadrado = calcular_cuadrado(num)"""
+    if (cuadrado := calcular_cuadrado(num)) %2 == 0:
+        lista_pares.append(cuadrado)
+        print(f"El cuadrado de {num} es {cuadrado}, es un numero par")
+    else:
+        print(f"El cuadrado de {num} es {cuadrado}, es un numeto impar")
+
+print(lista_pares)
